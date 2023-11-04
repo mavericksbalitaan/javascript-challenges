14706fb4582f2b1982adcf06c434b717ea7f9282
Mavericks Balitaan <balitaanmavericks@gmail.com>
2023-11-04 08:39:21 +0800

    Add solution to 41

--- 41-random-shuffle/41-script.js
+++ 41-random-shuffle/41-script.js
@@ -1,1 +1,14 @@
 // Create a function that shuffles the elements of an array randomly.
+
+const shuffleElements = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    // Generate a random index between 0 and i (inclusive)
+    const j = Math.floor(Math.random() * (i + 1));
+
+    // Swap elements at i and j
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+};
+
+console.log(shuffleElements(["M", "a", "v", "e", "r", "i", "c", "k", "s"]));
